58dd7de6ef6321bc6faa6538a45bb069cd39a3ac
YongMinSun <ddoni1010@gmail.com>
2019-11-06 22:05:11 +0900

    Initial commit

---
+++ app/src/main/java/com/tioeun/a20191106_02_playstore/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tioeun.a20191106_02_playstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
